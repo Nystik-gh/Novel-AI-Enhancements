8bc7649e37f5b7f52c7db146b27ba27853467e95
Nystik <236107-Nystik@users.noreply.gitlab.com>
2024-07-19 20:01:08 +0200

    fix url issue

--- core_test/core-test.user.js
+++ core_test/core-test.user.js
@@ -9,9 +9,6 @@
 // @require      https://github.com/Nystik-gh/Novel-AI-Enhancements/raw/core-script/core/dist/naie-core.user.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=novelai.net
 // @author       Nystik (https://gitlab.com/Nystik)
-// @downloadUrl  https://github.com/Nystik-gh/Novel-AI-Enhancements/raw/main/subshelves/dist/naie-subshelves.user.js
-// @homepageUrl  https://github.com/Nystik-gh/Novel-AI-Enhancements
-// @supportUrl   https://github.com/Nystik-gh/Novel-AI-Enhancements/issues
 // ==/UserScript==
 
 const NAIE = window.NAIE_INSTANCE
