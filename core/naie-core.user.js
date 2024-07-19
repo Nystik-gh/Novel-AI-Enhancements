7b42f925b82e9ec91a9d3b87500b2af2ea1c0e02
Nystik <236107-Nystik@users.noreply.gitlab.com>
2024-07-19 19:48:33 +0200

    testing userscript require behaviour

---
+++ core/naie-core.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// THIS IS NOT A STANDALONE USER SCRIPT! DO NO INSTALL THIS SCRIPT DIRECTLY.
+// @name         Novel AI Enhanced: Core
+// @namespace    github.nystik-hg
+// @version      0.1.0
+// @description  Core library
+// @require      ./modules/*
+// @require      ./lib/*
+// @author       Nystik (https://gitlab.com/Nystik)
+// ==/UserScript==
+
+// Ensure window.NAIE_INSTANCE is set to a new NAIE instance if not already set
+if (!window.NAIE_INSTANCE) {
+    console.log('creating naie instance')
+    window.NAIE_INSTANCE = createNAIEInstance()
+} else {
+    console.log('naie instance already exists, skipping')
+}
+
+const naieGlobalFunc = () => {
+    console.log('globally registered function')
+}
