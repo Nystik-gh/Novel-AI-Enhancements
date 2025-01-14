7f5ee26d753e7350f95e6cbc7d13f1a6d642dabf
Nystik <236107-Nystik@users.noreply.gitlab.com>
2025-01-14 19:29:19 +0100

    build dist

--- core/dist/naie-core.user.js
+++ core/dist/naie-core.user.js
@@ -2,12 +2,12 @@
 // THIS IS NOT A STANDALONE USER SCRIPT! DO NO INSTALL THIS SCRIPT DIRECTLY.
 // @name         Novel AI Enhanced: Core
 // @namespace    github.nystik-hg
-// @version      1.0.0
+// @version      1.0.1
 // @description  Core library
 // @author       Nystik (https://gitlab.com/Nystik)
 // ==/UserScript==
 
-(() => {
+;(() => {
     const wRef = unsafeWindow ? unsafeWindow : window
 
     /***
@@ -20,7 +20,7 @@
         if (!wRef.NAIE_INSTANCE) {
             logger.info('creating NAIE instance')
             wRef.NAIE_INSTANCE = createNAIEInstance()
-            
+
             // Start waiting for scripts to register and become ready
             internal_startWaitingForScripts()
         } else {
@@ -2243,6 +2243,32 @@ const createNAIEInstance = () => {
 
 
 
-    coreInit()
-})();
+    // Force a reload when the app navigates to or from /stories
+    // This is to make sure we only load the script when we access /stories
+
+    let previousPath = window.location.pathname
+    const handleUrlChange = () => {
+        const currentPath = window.location.pathname
+
+        if (
+            (previousPath.startsWith('/stories') && !currentPath.startsWith('/stories')) ||
+            (!previousPath.startsWith('/stories') && currentPath.startsWith('/stories'))
+        ) {
+            window.location.reload()
+        }
+
+        previousPath = currentPath
+    }
+
+    const observer = new MutationObserver(handleUrlChange)
+
+    observer.observe(document, { childList: true, subtree: true })
+
+    handleUrlChange() // Initial check
+
+    // Check if the current path is /stories before initializing
+    if (window.location.pathname.startsWith('/stories')) {
+        coreInit()
+    }
+})()
 
